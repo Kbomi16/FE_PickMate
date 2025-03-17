35353199c1a14bcc9633721ed213b630da20f2ce
Kbomi16 <kimbomi2172@naver.com>
2025-03-17 05:45:37 +0000

    ✨ feat: 파비콘 및 메타데이터 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Loading from '@/components/Loading'
 import { useAuthStore } from '@/store/authStore'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
@@ -42,12 +43,37 @@ export default function App({ Component, pageProps }: AppProps) {
   if (loading) return <Loading />
 
   return (
-    <div className="flex min-h-screen flex-col">
-      {!hideLayout && <Header />}
-      <main className="mt-20 flex-1">
-        <Component {...pageProps} />
-      </main>
-      {!hideLayout && <Footer />}
-    </div>
+    <>
+      <Head>
+        {/* 기본 메타데이터 */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <meta
+          name="description"
+          content="PickMate는 토이프로젝트를 시작하고 싶은 사람들과 함께할 팀원을 찾아주는 플랫폼이에요. 이제 혼자 고민하지 말고, 함께 팀을 이루어 재미있는 프로젝트를 진행해봐요."
+        />
+        <meta name="author" content="BuddyMate 팀" />
+        <title>PickMate</title>
+
+        {/* Open Graph 메타데이터 */}
+        <meta
+          property="og:title"
+          content="PickMate - 토이프로젝트 팀 매칭 플랫폼"
+        />
+        <meta
+          property="og:description"
+          content="PickMate는 토이프로젝트를 시작하고 싶은 사람들과 함께할 팀원을 찾아주는 플랫폼이에요. 이제 혼자 고민하지 말고, 함께 팀을 이루어 재미있는 프로젝트를 진행해봐요."
+        />
+        <meta property="og:image" content="/assets/imgs/logo.png" />
+        <meta property="og:url" content="https://fe-pick-mate.vercel.app/" />
+      </Head>
+      <div className="flex min-h-screen flex-col">
+        {!hideLayout && <Header />}
+        <main className="mt-20 flex-1">
+          <Component {...pageProps} />
+        </main>
+        {!hideLayout && <Footer />}
+      </div>
+    </>
   )
 }
